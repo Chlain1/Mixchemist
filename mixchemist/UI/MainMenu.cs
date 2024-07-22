52a332ccd5444197b6d2f7fee5764ffd7d4605d8
Eric Zahn <eric.zahn@uni-ulm.de>
2024-07-22 17:25:13 +0200

    feat: basic Main Menu with connection to DevScene

---
+++ mixchemist/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnExitButtonPressed()
+	{
+		Debug.WriteLine("Quitting game");
+		GetTree().Quit();
+	}
+
+	private void OnStartButtonPressed()
+	{
+		Debug.WriteLine("Start Game");
+		GetTree().ChangeSceneToFile("res://devTesting/DevScene.tscn");
+	}
+    
+}
