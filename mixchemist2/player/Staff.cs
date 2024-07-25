db8d53d9600aaea77502a4343ed3f25cc4e721de
Michail <michail.magazov@uni-ulm.de>
2024-07-25 17:39:16 +0200

    feat: add extreme depression to project

--- mixchemist2/player/Staff.cs
+++ mixchemist2/player/Staff.cs
@@ -4,6 +4,7 @@ using static ClassesAndEnums;
 
 public partial class Staff : Node2D
 {
+    [Export] private PackedScene playerInputScene;
     [Export] private PackedScene elementStorageScene;
     [Export] private PackedScene bulletScene;
     [Export] float bulletSpeed = 1500f;
@@ -11,9 +12,7 @@ public partial class Staff : Node2D
     [Export] float damage = 10;
 
     private ElementStorage elementStorage;
-
-    //[Signal]
-    //public delegate void StoreSpell(Element storedElement);
+    private Label playerInput;
     
     private Element currentElement;
     private float fireRate;
@@ -23,29 +22,33 @@ public partial class Staff : Node2D
     public override void _Ready()
     {
         elementStorage = (ElementStorage)elementStorageScene.Instance();
+        playerInput = (Label)playerInputScene.Instance();
         fireRate = 1 / bps;
-        //Connect(nameof(StoreSpell), elementStorage, nameof(ElementStorage.StoreSpellColor));
     }
 
     public override void _Input(InputEvent @event)
     {
         if (@event.IsActionPressed("w_cast"))
         {
+            playerInput.Text += "W";
             currentElement = ClassesAndEnums.Element.FIRE;
             spellReadyToCast = true;
         }
         else if (@event.IsActionPressed("a_cast"))
         {
+            playerInput.Text += "A";
             currentElement = ClassesAndEnums.Element.EARTH;
             spellReadyToCast = true;
         }
         else if (@event.IsActionPressed("d_cast"))
         {
+            playerInput.Text += "D";
             currentElement = ClassesAndEnums.Element.WATER;
             spellReadyToCast = true;
         }
         else if (@event.IsActionPressed("s_cast"))
         {
+            playerInput.Text += "S";
             currentElement = ClassesAndEnums.Element.AIR;
             spellReadyToCast = true;
         }
