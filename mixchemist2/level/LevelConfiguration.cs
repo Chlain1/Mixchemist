fefbe164461aaad58d1c54add22158dd171885f6
SaltDispenser <kranz.georg@web.de>
2024-07-28 14:01:10 +0200

    fix: implemented target_fps in LevelConfiguration

--- mixchemist2/level/LevelConfiguration.cs
+++ mixchemist2/level/LevelConfiguration.cs
@@ -7,11 +7,14 @@ using static ClassesAndEnums;
 public class LevelConfiguration : Node2D
 {
     [Export] private List<Element> allowedBasicElements = new List<Element>();
+
+    private int target_fps = 60;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         Debug.WriteLine(this.Name);
+        Engine.TargetFps = target_fps;
         
         // P.S: Ich weiß das ist extrem dämlich, aber es funktioniert xoxo Eric
         switch (this.Name)
